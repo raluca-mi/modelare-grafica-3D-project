5e51fcb7d9c2197105ece2559dc4f8561e97aa6b
RaresNeagu <77065057+RaresNeagu@users.noreply.github.com>
2021-12-15 19:37:54 +0200

    Added Mipmaps to textures

--- Project1/Project1/Texture.cpp
+++ Project1/Project1/Texture.cpp
@@ -19,13 +19,16 @@ Texture::Texture(const std::string& path)
 	GlCall(glGenTextures(1, &m_RendererId));
 	GlCall(glBindTexture(GL_TEXTURE_2D, m_RendererId));
 
-	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+	
+	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR));
 	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
 	GlCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
 
 	GlCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer));
+	GlCall(glGenerateMipmap(GL_TEXTURE_2D));
 	GlCall(glBindTexture(GL_TEXTURE_2D, 0));
+	
 
 	if (m_LocalBuffer)
 		stbi_image_free(m_LocalBuffer);
